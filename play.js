170284f49e55d9a3686129ab9a866d569d282637
Hope Warren <hopevaughnwarren@gmail.com>
2022-06-13 13:59:31 -0700

    added input.js as module for export. tested,working,linted

--- play.js
+++ play.js
@@ -1,23 +1,9 @@
 // client side server
 const { connect } = require('./client');
+const { setupInput } = require('./input')
 
-// setup interface to handle user input from stdin
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
 
 console.log("Connecting ...");
 connect();
+
 setupInput();
