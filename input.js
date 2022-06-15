9c369c5a130c452381c7871f34282b1df891feac
Hope Warren <hopevaughnwarren@gmail.com>
2022-06-14 18:58:50 -0700

    Updated input function to bind the movememnt keys. passed the resulting object from client to setinput function. tested, working. linted

--- input.js
+++ input.js
@@ -1,17 +1,35 @@
-// setup interface to handle user input from stdin
+// setup interface to handle user input from stdinlet connection;
+let connection;
+
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w' || key === 'W') {
+    connection.write('Move: up');
+    console.log('Move: up');
+  }
+  if (key === 's' || key === 'S') {
+    connection.write('Move: down');
+    console.log(`Move: down`);
+  }
+  if (key === 'a' || key === 'A') {
+    connection.write('Move: left');
+    console.log(`Move: left`);
+  }
+  if (key === 'd' || key === 'D') {
+    connection.write('Move: right');
+    console.log(`Move: right`);
+  }
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', handleUserInput);
-  return stdin;
 };
 
 module.exports = {
--- play.js
+++ play.js
@@ -1,9 +1,8 @@
 // client side server
 const { connect } = require('./client');
-const { setupInput } = require('./input')
+const { setupInput } = require('./input');
 
 
 console.log("Connecting ...");
-connect();
-
-setupInput();
+const obj = connect();
+setupInput(obj);
